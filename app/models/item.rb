0f746cef9535455e012d8b145cb2490875099fe9
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 13:51:36 -0500

    Create method for valid items and replace logic in view.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :category
 
-  def expires
-    Date.today + shelf_life_days.days
+  def expires_on
+    created_at + shelf_life_days.days
+  end
+
+  def self.valid_items
+    Item.all.select { |item| item.expires_on >= Date.today }
   end
 end

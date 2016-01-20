8f254bad366ecda4357401accab9d14661ba51dd
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 13:58:20 -0500

    Add test coverages for expires_on method.

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     item.price 10.00
     item.quantity 17
     item.shelf_life_days 14
+    item.created_at Date.today
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
 
   let(:item) {FactoryGirl.create(:item)}
-  let(:item2) {FactoryGirl.create(:item, shelf_life_days: -1)}
+  let(:item2) {FactoryGirl.create(:item, created_at: Date.today, shelf_life_days: -1)}
 
   describe '.valid_items' do
 
@@ -14,6 +14,15 @@ RSpec.describe Item, type: :model do
     it 'does not return expired items' do
       expect(Item.valid_items).to_not include item2
     end
+  end
+
+  describe '#expires_on' do
+
+    it 'returns an expiration date for an item' do
+      expect(item.expires_on).to eq (Date.today + 14.days)
+      expect(item2.expires_on).to eq (Date.today - 1.days)
+    end
+
   end
 
 end

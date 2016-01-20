855656cb38763cd11e1dd23027e206adb68c0c5b
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 14:42:16 -0500

    Put items home route into collection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'items#home'
 
-  resources :items, only: [:create, :update, :home]
+  resources :items, only: [:create, :update] do  
+    collection do
+      get :home
+    end
+  end
   resources :categories, only: [:create]
 end
--- spec/factories/items.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :item do |item|
-    item.name "An item"
-    item.price 10.00
-    item.quantity 17
-    item.shelf_life_days 4
-  end
-end

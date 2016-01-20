9fc01bc18ddd79c59f836910f24f5d652cb7d6af
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 12:36:42 -0500

    Delete singleton syntax for route already defined in root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'items#home'
-
-  get '/home', to: 'items#home'
+  
   resources :items, only: [:create, :update]
   resources :categories, only: [:create]
 end

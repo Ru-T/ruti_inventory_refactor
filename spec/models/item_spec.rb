905e917343d0dcbed1c612abb9841b54ac3bc362
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 17:18:38 -0500

    Met the HOUND

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
 
-  let!(:item) {FactoryGirl.create(:item)}
-  let(:item2) {FactoryGirl.create(:item, price: 3,
-              created_at: Date.today, shelf_life_days: -1)}
+  let!(:item) { FactoryGirl.create(:item) }
+  let(:item2) { FactoryGirl.create(:item,
+                                    price: 3,
+                                    created_at: Date.today,
+                                    shelf_life_days: -1) }
 
   describe 'validations' do
 	  it "is valid with all fields" do
@@ -29,24 +31,24 @@ RSpec.describe Item, type: :model do
   end
 
   describe '.valid_items' do
-    it 'returns all items that have not expired' do
+    it "returns all items that have not expired" do
       expect(Item.valid_items).to include item
     end
 
-    it 'does not return expired items' do
+    it "does not return expired items" do
       expect(Item.valid_items).to_not include item2
     end
   end
 
-  describe '#expires_on' do
-    it 'returns an expiration date for an item' do
+  describe "#expires_on" do
+    it "returns an expiration date for an item" do
       expect(item.expires_on).to eq (Date.today + 14.days)
       expect(item2.expires_on).to eq (Date.today - 1.days)
     end
   end
 
-  describe '#price_in_dollars' do
-    it 'returns a dollar amount for price' do
+  describe "#price_in_dollars" do
+    it "returns a dollar amount for price" do
       expect(item.price_in_dollars).to eq '0.10'
       expect(item2.price_in_dollars).to eq '0.03'
     end

907fa5535725bc4bc51faf840949d4ebfb02f7b0
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-20 14:08:40 -0500

    Add update to items, not categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#home'
 
-  resources :items, only: [:create, :home]
-  resources :categories, only: [:create, :update]
+  resources :items, only: [:create, :update, :home]
+  resources :categories, only: [:create]
 end

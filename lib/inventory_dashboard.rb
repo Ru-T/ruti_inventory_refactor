e40a273e83f67b7ceeef9b8e7f36ba69f0d1cc58
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-21 12:37:18 -0500

    Move dashboard to lib folder since it is not a model.

---
+++ lib/inventory_dashboard.rb
@@ -0,0 +1,16 @@
+class InventoryDashboard < ActiveRecord::Base
+  attr_reader :items
+
+  def initialize(items)
+    @items = Item.valid_items
+  end
+
+  def new_item(attrs)
+    @new_item = Item.new(attrs)
+  end
+
+  def new_category(attrs)
+    @category = Category.new(attrs)
+  end
+
+end

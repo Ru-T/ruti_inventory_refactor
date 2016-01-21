b8c72efc2e4e4a81f0946373c82212e121b1293e
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-21 16:29:00 -0500

    Change items to items from inventory dashboard on index view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def create
-    @inventory_manager = InventoryDashboard.new(Item.all)
-    @inventory_manager.save_category(category_params)
+    @inventory_dashboard = InventoryDashboard.new(Item.all)
+    @inventory_dashboard.save_category(category_params)
     redirect_to root_path
     end
   end

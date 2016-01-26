41081e3e366b2c0696f57954ab12b156abe29022
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-26 15:19:43 -0500

    Add test coverage for incrementing the count of an item or category.

--- spec/lib/inventory_dashboard_spec.rb
+++ spec/lib/inventory_dashboard_spec.rb
@@ -4,6 +4,8 @@ require "inventory_dashboard.rb"
 RSpec.describe InventoryDashboard do
   let!(:item) { FactoryGirl.create(:item) }
   let!(:category) { FactoryGirl.create(:category) }
+  let!(:new_item) { subject.new_item(name: "N", shelf_life_days: 2, quantity: 1, price: 2) }
+  let!(:new_category) { subject.new_category(name: "Category2") }
 
   subject { InventoryDashboard.new(Item.all) }
 
@@ -15,9 +17,8 @@ RSpec.describe InventoryDashboard do
 
   describe "#new_item" do
     it "creates a new item" do
-      new_item = subject.new_item(name: "N", shelf_life_days: 2, quantity: 1, price: 2)
       expect(new_item).to respond_to(:save)
-      expect{ new_item }.to change { Item.count }.by(1)
+      expect{ new_item.save }.to change { Item.count }.by(1)
     end
   end
 
@@ -29,9 +30,8 @@ RSpec.describe InventoryDashboard do
 
   describe "#new category" do
     it "creates a new category" do
-      new_category = subject.new_category(name: "Category2")
       expect(new_category).to respond_to(:save)
-      expect{ new_category }.to change { Category.count }.by(1)
+      expect{ new_category.save }.to change { Category.count }.by(1)
     end
   end
 end
